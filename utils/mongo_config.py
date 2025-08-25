37eea09bf9cdc314efb97d10169bada8e2cc385c
Avanish Ojha <85388688+Avanish08@users.noreply.github.com>
2025-08-25 18:49:38 +0530

    Update mongo_config.py

--- utils/mongo_config.py
+++ utils/mongo_config.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://akdis0302:B1xvoFJIp6V88PrS@cluster0.ydzfxbm.mongodb.net/")
+client = MongoClient("YOUR MONOGO DB ADDRESS")
 db = client["attendance_db"]
 students_col = db["students"]
 attendance_col = db["attendance"]
 users_col = db["users"]
 timetables_col = db["timetables"]
 subjects_col = db["subjects"]
+
 
